08bc611ea6462024f621fb1e41f7b2c8860ac08a
Katrina Ornales <kat.ornales@paymaya.com>
2018-10-14 23:16:42 +0800

    add user data and tags

--- instance.tf
+++ instance.tf
@@ -1,4 +1,11 @@
 resource "aws_instance" "kat-tf" {
     ami = "ami-3ed28542"
     instance_type = "t2.micro"
+    tags {
+        CostCodeCategory = "Acquiring Payment Processor"
+    }
+    user_data = <<-EOF
+                    #!/bin/bash
+                    echo Hello World!
+                EOF
 }
